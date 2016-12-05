87ffdb111c31da9e38650758e1e32d753ad07180
Kostia Grinchik <neaved15@gmail.com>
2016-12-05 22:46:55 +0300

    guiv2.0 ClientMenSer & ProviderMenSer

--- TP/Bookmaker.Designer.cs
+++ TP/Bookmaker.Designer.cs
@@ -28,8 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("dfsdsfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
-        "aaaaaaaaaaaaaaaaaaaaaaaaaa", "sdfg");
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.listView1 = new System.Windows.Forms.ListView();
@@ -64,14 +62,24 @@
             this.id,
             this.Meneger,
             this.Payed});
-            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem1});
             this.listView1.Location = new System.Drawing.Point(18, 51);
             this.listView1.Name = "listView1";
             this.listView1.Size = new System.Drawing.Size(634, 211);
             this.listView1.TabIndex = 7;
             this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // id
+            // 
+            this.id.Width = 161;
+            // 
+            // Meneger
+            // 
+            this.Meneger.Width = 193;
+            // 
+            // Payed
+            // 
+            this.Payed.Width = 183;
             // 
             // Bookmaker
             // 
